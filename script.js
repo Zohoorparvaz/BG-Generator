72d96945dbf838c5b12d359695e211bf95acd845
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-02-18 21:18:05 -0700

    added more files

---
+++ script.js
@@ -0,0 +1,14 @@
+var css = document.querySelector("h3");
+var color1 = document.getElementById("color1");
+var color2 = document.getElementById("color2");
+var theBody = document.getElementById("the-body");
+
+console.log("bg");
+
+// function setGradient() {
+// }
+
+color1.addEventListener("click", function () {
+  console.log("click");
+})
+
